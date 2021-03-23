bcbf984d5acfab59b8c4a2b92d6b35aa1b9a23d0
Anastasiya <infgai@yandex.ru>
2021-03-23 18:31:12 +0300

    add for to main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/<name>')
-def index(name=None):
-    return render_template('index.html', name='qqqqqqqqqqqqqqqqqqq')
+@app.route('/')
+def index(name=None, items=None):
+    poetry = ['картина', 'корзина', 'картонка']
+    return render_template('index.html', name='qqqqqqqqqqqqqqqqqqq', items=poetry)
 
 
 app.run('127.0.0.1', 8080, debug=True)
